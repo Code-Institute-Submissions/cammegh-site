edee92bf30c6dfcb4521dbeef1ed7edb5f12ceb6
Seb James <sebjames5@gmail.com>
2019-01-18 10:58:26 +0000

    Added pictures and hover state to roulette wheels page. Changed styling

--- js/product-details.js
+++ js/product-details.js
@@ -2,12 +2,16 @@ $(function() {
     $('.product-button').on('click', function() {
         function toggleButtonOff(param) {
             var divs = param.find('.active-product-content');
+            var path = param.find('.tile').attr('src');
             divs.each(function() {
                 $(this).slideUp(500);
             });
             param.parent().removeClass('active-product');
             param.find($('.descriptor')).first().delay(900).fadeIn(300);
             param.find($('.descriptor')).last().fadeOut(500);
+            setTimeout(function(){
+                param.css('background-image','url("'+path+'")');
+            },900);
         }
         if ($(this).parent().hasClass('active-product')) {
             toggleButtonOff($(this));
@@ -19,6 +23,7 @@ $(function() {
                 }
             });
             $(this).parent().addClass('active-product');
+            $(this).css('background-image','inherit');
             $(this).find($('.descriptor')).first().fadeOut(500);
             $(this).find($('.descriptor')).last().delay(500).fadeIn(500);
             var divs = $(this).find('.active-product-content');
